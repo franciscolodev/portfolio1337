8f8fba32859a151650deee36a99c002b09b64518
Fran <franciscolodev@gmail.com>
2025-04-10 02:26:08 -0300

    Primer commit

---
+++ src/lib/data.ts
@@ -0,0 +1,128 @@
+export const personalInfo = {
+  name: "Francisco López",
+  email: "coder@franciscolodev.com",
+  github: "https://github.com/franciscolodev",
+  linkedin: "https://www.linkedin.com/in/franciscolodev/",
+  location: "Río Cuarto, Córdoba, Argentina",
+};
+
+export const workExperience = [
+  {
+    company: "Freelance",
+    location: "Remote",
+    position: "Web Developer",
+    period: "Oct 2023 - Present",
+    achievements: [
+      "Freelancer Web development using HTML5, CSS3, JavaScript, Node.js, Astro.js + other frameworks and libraries.",
+    ],
+  },
+  {
+    company: "Professional profile",
+    location: "Remote",
+    position: "Web Developer",
+    period: "Feb 2025 - Present",
+    achievements: [
+      "Web development for the professional profile of a medical propaganda agent using Astro.js and Tailwind CSS.",
+    ],
+  },
+  {
+    company: "Nicolo's Barbershop",
+    location: "Río Cuarto, Córdoba, Argentina",
+    position: "Web Developer",
+    period: "Nov 2024 - Ene 2025",
+    achievements: [
+      "Developed responsive frontend using HTML5, CSS3 and JavaScript",
+      "Optimized JavaScript solutions to improve load times and data-fetching efficiency.",
+    ],
+  },
+  {
+    company: "UNAGI-Shop",
+    location: "Remote",
+    position: "Web Developer",
+    period: "Mar 2024 - Abr 2024",
+    achievements: [
+      "Website development for an online store specializing in graphic design and production in all its forms (screen printing, DTF, embroidery, and 3D printing).",
+      "Using HTML5, CSS3 and JavaScript.",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    title: "Professional profile",
+    github: "https://franciscolodev.com",
+    description: [
+      "In process",
+      "Web development for the professional profile of a medical propaganda agent using Astro.js and Tailwind CSS.",
+    ],
+  },
+  {
+    title: "Nicolo's Barbershop",
+    github: "https://portfoliofranciscolodev.vercel.app/",
+    description: [
+      "Developed responsive frontend using HTML5, CSS3 and JavaScript",
+      "Optimized JavaScript solutions to improve load times and data-fetching efficiency.",
+    ],
+  },
+  {
+    title: "UNAGI-Shop",
+    github: "https://franciscolodev.github.io/UNAGI/productos.html",
+    description: [
+      "Website development for an online store specializing in graphic design and production in all its forms (screen printing, DTF, embroidery, and 3D printing).",
+      "Using HTML5, CSS3 and JavaScript.",
+    ],
+  },
+  {
+    title: "Fruta é Fruto",
+    github: "https://franciscolodev.github.io/Fruta-e-Fruto/",
+    description: [
+      "Web development and maintenance for a store dedicated to the sale of fruits and vegetables",
+    ],
+  },
+];
+
+export const skills = {
+  programmingLanguages: [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+  ],
+  frontendDevelopment: [
+    "HTML5",
+    "CSS3",
+    "Astro.js",
+    "Tailwind CSS",
+    "React.js",
+  ],
+  backendDevelopment: ["Node.js"],
+  toolsAndServices: [
+    "VSCode",
+    "Git",
+    "GitHub",
+    "Vercel",
+    "Hostinger",
+  ],
+};
+
+
+export const education = [
+  {
+    institution: "Universidad Tecnológica Nacional (UTN)",
+    location: "Argentina",
+    degree: "Tecnicatura Universitaria en Programación",
+    period: "Mar 2025 - Present",
+    achievements: [
+      "Universitary Technical in Programming",
+    ],
+  },
+  {
+    institution: "Alura",
+    location: "Latam",
+    degree: "ONE (Oracle Next Education) Program",
+    period: "Feb 2023 - Dic 2023",
+    achievements: [
+      "Through Alura Latam in partnership with Oracle Co. as a Front-End Developer",
+      "Training in HTML5, CSS3, JavaScript, and React.",
+    ],
+  },
+];
